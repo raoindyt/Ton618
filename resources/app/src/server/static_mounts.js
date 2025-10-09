e95cb0d49ab2ba3b5c08810a6eb50618ef02ef5f
raoindyt <oleksejmudalo@gmail.com>
2025-10-09 22:27:18 +0200

    0.1.2

--- resources/app/src/server/static_mounts.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-export function mountStatic(app){
-  // Expose covers and library directories under /data
-  app.use('/data/covers', express.static(app.get('COVER_DIR')));
-  app.use('/data/library', express.static(app.get('LIB_DIR')));
-}
